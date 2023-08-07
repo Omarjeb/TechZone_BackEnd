5d0df63a9c332424f3efe583e3a92bd1ceeebbe4
omarjeb <omar.jebali@outlook.com>
2023-08-07 15:48:09 +0100

    first commit

---
+++ utils/cloudinary.js
@@ -0,0 +1,42 @@
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: "dk91b8jpl",
+  api_key: "382116823331398",
+  api_secret: "pK2QoY5vPds5EVf5h0d93zAhaqg",
+});
+
+const cloudinaryUploadImg = async (fileToUploads) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUploads, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        },
+        {
+          resource_type: "auto",
+        }
+      );
+    });
+  });
+};
+const cloudinaryDeleteImg = async (fileToDelete) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.destroy(fileToDelete, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        },
+        {
+          resource_type: "auto",
+        }
+      );
+    });
+  });
+};
+
+module.exports = { cloudinaryUploadImg, cloudinaryDeleteImg };
